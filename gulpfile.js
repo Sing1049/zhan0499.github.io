9973c292d43774ed8ad975faa46dedb4a23b3663
Douglas Zhang <zhan0499@algonquinlive.com>
2019-02-27 19:45:27 -0500

    After Sass Breakdown

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,25 @@
-const gulp = require('gulp') // load gulp
-const sass = require('gulp-sass') // load gulp-sass
-const browserSync = require('browser-sync').create() // load browser-sync and create an instance
-const postcss = require('gulp-postcss') // load the postcss library
-const autoprefixer = require('autoprefixer') // load the autoprefixer plugin
-const cssnano = require('cssnano') // load the cssnano plugin
-// Define a task to compile Sass and run autoprefixer and cssnano
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+
 gulp.task('sass', function () {
   const plugins = [
-    autoprefixer({ browsers: ['last 2 version'] }),
+    autoprefixer({browsers:['last 2 version']}),
     cssnano()
   ]
   return gulp
-    .src('scss/**/*.scss') // source of any sass files
-    .pipe(sass()) // run the sass compiler on the source file
-    .pipe(gulp.dest('css')) // destination for the compiled css files
-    .pipe(postcss(plugins)) // apply the PostCSS plugins
-    .pipe(gulp.dest('./css/min')) // path to output the minified css file
-    .pipe(browserSync.stream()) // run the browsersync stream
+    .src('scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('css/min'))
+    .pipe(browserSync.stream())
 })
-// Define the default task
 gulp.task('default', function () {
-  // initialize browserSync on the current folder
   browserSync.init({ server: './' })
-  // watch for changes to any files in scss folder and its sub folders and with .scss extension, run the sass task when a change is found
   gulp.watch('scss/**/*.scss', gulp.series('sass'))
-  // watch for changes on any .html file and reload the browser on change
   gulp.watch('*.html').on('change', browserSync.reload)
 })
